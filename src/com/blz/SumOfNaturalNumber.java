eaff43588b3255efe14c7e31ed0ae8d4ba597458
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-01 13:53:27 +0530

    Sum Of Natural Number For While Loop

---
+++ src/com/blz/SumOfNaturalNumber.java
@@ -0,0 +1,16 @@
+package com.blz;
+
+public class SumOfNaturalNumber {
+    public static void main(String[] args) {
+        int n =5;
+        int sum =0;
+        int i =1;
+
+        while(i <= 5){
+            sum +=i;
+            System.out.println(sum);
+            i++;
+        }
+    }
+}
+
